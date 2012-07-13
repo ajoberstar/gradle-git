32a1db16c3e63102a20330efab1c3012b6ebc8da
Andrew Oberstar <ajoberstar@gmail.com>
2012-07-13 17:23:30 -0500

    Updating the GitTag task to support force and sign props.

--- src/main/groovy/org/ajoberstar/gradle/git/tasks/GitTag.java
+++ src/main/groovy/org/ajoberstar/gradle/git/tasks/GitTag.java
@@ -1,101 +1,178 @@
+/* Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ajoberstar.gradle.git.tasks;
 
 import groovy.lang.Closure;
-import org.gradle.api.tasks.TaskAction;
-import org.eclipse.jgit.api.CommitCommand;
-import org.gradle.api.GradleException;
+
+import org.ajoberstar.gradle.util.ObjectUtil;
 import org.eclipse.jgit.api.TagCommand;
+import org.eclipse.jgit.api.errors.ConcurrentRefUpdateException;
+import org.eclipse.jgit.api.errors.InvalidTagNameException;
+import org.eclipse.jgit.api.errors.NoHeadException;
 import org.eclipse.jgit.lib.PersonIdent;
+import org.gradle.api.GradleException;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.Optional;
+import org.gradle.api.tasks.TaskAction;
 import org.gradle.util.ConfigureUtil;
-import org.ajoberstar.gradle.util.ObjectUtil;
 
+/**
+ * Task to tag a commit in a Git repository.
+ * @since 0.2.0
+ */
 public class GitTag extends GitBase {
-  private PersonIdent tagger = null;
-  private Object message = null;
-  private Object tagName = null;
+	private Object tagName = null;
+	private Object message = null;
+	private boolean sign = false;
+	private boolean force = false;
+	private PersonIdent tagger = null;
 
-  /**
-   * Tags the HEAD.
-   */
-  @TaskAction
-  public void tag() {
-    TagCommand cmd = getGit().tag();
-    if (message != null) {
-      cmd.setMessage(getMessage());
-    }
-    if (tagger != null) {
-      cmd.setTagger(getTagger());
-    }
-    cmd.setName(getTagName());
-    try {
-      cmd.call();
-    } catch (Exception e) {
-      throw new GradleException("Problem tagging revision.", e);
-    }
-  }
+	/**
+	 * Tags the HEAD.
+	 */
+	@TaskAction
+	public void tag() {
+		TagCommand cmd = getGit().tag();
+		cmd.setName(getTagName());
+		cmd.setMessage(getMessage());
+		cmd.setSigned(getSign());
+		cmd.setForceUpdate(getForce());
+		if (tagger != null) {
+			cmd.setTagger(getTagger());
+		}
+		
+		try {
+			cmd.call();
+		} catch (ConcurrentRefUpdateException e) {
+			throw new GradleException("Another process is accessing or updating the ref.", e);
+		} catch (InvalidTagNameException e) {
+			throw new GradleException("Invalid tag name: " + getTagName(), e);
+		} catch (NoHeadException e) {
+			throw new GradleException("Cannot tag without a HEAD revision.", e);
+		}
+	}
 
-  @Input
-  @Optional
-  public PersonIdent getTagger() {
-    return tagger;
-  }
+	/**
+	 * Gets the tag message to use.
+	 * @return the tag message to use
+	 */
+	@Input
+	@Optional
+	public String getMessage() {
+		return ObjectUtil.unpackString(message);
+	}
 
-  /**
-   * Sets the tagger.
-   * @param tagger the tagger
-   */
-  public void setTagger(PersonIdent tagger) {
-    this.tagger = tagger;
-  }
+	/**
+	 * Sets the tag message to use.
+	 * @param message the tag message
+	 */
+	public void setMessage(Object message) {
+		this.message = message;
+	}
 
-  /**
-   * Configures the tagger.
-   * A {@code PersonIdent} is passed to the closure.
-   * @param config the configuration closure
-   */
-  @SuppressWarnings("rawtypes")
-  public void tagger(Closure config) {
-    if (tagger == null) {
-      this.tagger = new PersonIdent(getGit().getRepository());
-    }
-    ConfigureUtil.configure(config, tagger);
-  }
+	/**
+	 * Gets the tag name to use.
+	 * @return the tag name to use
+	 */
+	@Input
+	public String getTagName() {
+		return ObjectUtil.unpackString(tagName);
+	}
 
-  /**
-   * Gets the tag message to use.
-   * @return the tag message to use
-   */
-  @Input
-  @Optional
-  public String getMessage() {
-    return ObjectUtil.unpackString(message);
-  }
+	/**
+	 * Sets the tag name to use.
+	 * @param tagName the tag name
+	 */
+	public void setTagName(Object tagName) {
+		this.tagName = tagName;
+	}
+	
+	/**
+	 * Gets whether or not the tag will be signed with
+	 * the tagger's default key.
+	 * 
+	 * This defaults to false.
+	 * @return {@code true} if the tag will be signed,
+	 * {@code false} otherwise
+	 */
+	@Input
+	public boolean getSign() {
+		return sign;
+	}
+	
+	/**
+	 * Sets whether or not the tag will be signed
+	 * by the tagger's default key.
+	 * @param sign {@code true} if the tag should 
+	 * be signed, {@code false} otherwise
+	 */
+	public void setSign(boolean sign) {
+		this.sign = sign;
+	}
+	
+	/**
+	 * Gets whether or not the tag will be created/updated even
+	 * if a tag of that name already exists.
+	 * 
+	 * This defaults to false.
+	 * @return {@code true} if the tag will be forced,
+	 * {@code false} otherwise
+	 */
+	@Input
+	public boolean getForce() {
+		return force;
+	}
+	
+	/**
+	 * Sets whether or not the tag will be created/updated
+	 * even if a tag of that name already exists.
+	 * @param force {@code true} if the tag will be forced,
+	 * {@code false} otherwise
+	 */
+	public void setForce(boolean force) {
+		this.force = force;
+	}
+	
+	/**
+	 * Gets the tagger.
+	 * @return the tagger
+	 */
+	@Input
+	@Optional
+	public PersonIdent getTagger() {
+		return tagger;
+	}
 
-  /**
-   * Sets the tag message to use.
-   * @param message the tag message
-   */
-  public void setMessage(Object message) {
-    this.message = message;
-  }
+	/**
+	 * Sets the tagger.
+	 * @param tagger the tagger
+	 */
+	public void setTagger(PersonIdent tagger) {
+		this.tagger = tagger;
+	}
 
-  /**
-   * Gets the tag name to use.
-   * @return the tag name to use
-   */
-  @Input
-  public String getTagName() {
-    return ObjectUtil.unpackString(tagName);
-  }
-
-  /**
-   * Sets the tag name to use.
-   * @param tagName the tag name
-   */
-  public void setTagName(Object tagName) {
-    this.tagName = tagName;
-  }
-
+	/**
+	 * Configures the tagger.
+	 * A {@code PersonIdent} is passed to the closure.
+	 * @param config the configuration closure
+	 */
+	@SuppressWarnings("rawtypes")
+	public void tagger(Closure config) {
+		if (tagger == null) {
+			this.tagger = new PersonIdent(getGit().getRepository());
+		}
+		ConfigureUtil.configure(config, tagger);
+	}
 }
