9f70cf02b966b75d9ab7e134b51c882eabc8077b
Andrew Oberstar <ajoberstar@gmail.com>
2014-04-07 19:28:37 -0500

    Adding javadoc to inferred version.

--- src/main/groovy/org/ajoberstar/gradle/git/semver/InferredVersion.groovy
+++ src/main/groovy/org/ajoberstar/gradle/git/semver/InferredVersion.groovy
@@ -22,22 +22,93 @@ import org.ajoberstar.grgit.Grgit
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+/**
+ * Version object that infers its value from the existing Git tags in the
+ * repository and the stage and scope of the change.
+ *
+ * <p>
+ * Version will be inferred by finding the nearest tagged version using
+ * {@link org.ajoberstar.gradle.git.semver.NearestVesrionLocator}. Based on
+ * the scope of the changes and the stage of their completion (both of which
+ * must be provided to the {@link #infer(String, String)} method) the nearest
+ * version will be incremented appropriately to result in the inferred version.
+ * </p>
+ *
+ * <p>
+ * If the closest tagged version is v1.1.0 (for the normal version)
+ * and the closes absolute version is v1.2.0-milestone.1, here are some examples
+ * of the versions that would be inferred (assuming the default settings on
+ * this object):
+ * </p>
+ *
+ * <table>
+ *   <tr><td>Scope</td><td>Stage</td><td>Inferred Version</td></tr>
+ *   <tr><td>patch</td><td>milestone</td><td>1.1.1-milestone.1</td></tr>
+ *   <tr><td>minor</td><td>dev</td><td>1.2.0-dev.&lt;commits since 1.1.0&gt;</td></tr>
+ *   <tr><td>minor</td><td>milestone</td><td>1.2.0-milestone.2</td></tr>
+ *   <tr><td>minor</td><td>rc</td><td>1.2.0-rc.1</td></tr>
+ *   <tr><td>minor</td><td>final</td><td>1.2.0</td></tr>
+ *   <tr><td>major</td><td>milestone</td><td>2.0.0-milestone.1</td></tr>
+ * </table>
+ *
+ * @since 0.8.0
+ */
 class InferredVersion {
 	private static final Logger logger = LoggerFactory.getLogger(InferredVersion)
 	private Version inferredVersion = null
 
+	/**
+	 * The git repository to infer from.
+	 */
 	Grgit grgit
+
+	/**
+	 * Valid stages for versions that should not have tags created for them.
+	 * These stages will be numbered based on the number of commits since the
+	 * nearest normal version rather than the number of releases for the
+	 * inferred normal verison and stage. Defaults to ['dev'].
+	 */
 	SortedSet<String> untaggedStages = ['dev'] as SortedSet
+
+	/**
+	 * Valid stages for versions that should have tags created for them. These
+	 * stages will be numbered in incrementing fashion based on the number of
+	 * the nearest absolute version, if it has the same normal version and stage.
+	 * Defaults to ['milestone', 'rc'].
+	 */
 	SortedSet<String> taggedStages = ['milestone', 'rc'] as SortedSet
 
-
+	/**
+	 * Closure to determine whether build metadata should be included in the
+	 * inferred version. Should accept a single String argument for
+	 * the stage. Should return true or false. Defaults to returning true for
+	 * stages besides 'final' and false otherwise.
+	 */
 	Closure<Boolean> useBuildMetadataForStage = { stage -> stage != 'final' }
+
+	/**
+	 * Closure to create the build metadata to use in the inferred versions.
+	 * Only exected for stages that meet {@link #useBuildMetadataForStage}.
+	 * Will called with no arguments and should return a String representing
+	 * the build metadata for this version.
+	 */
 	Closure<String> createBuildMetadata = { grgit.head().id }
 
+	/**
+	 * Infers the version using the given arguments.
+	 * @param scope the scope of the change. Must be a string matching one of
+	 * {@link #ChangeScope}'s values
+	 * @param stage one of the valid stages for this object. See {@link #getAllStages()}
+	 */
 	void infer(String scope, String stage) {
 		infer(ChangeScope.valueOf(scope.toUpperCase()), stage)
 	}
 
+	/**
+	 * Infers the version using the given arguments.
+	 * @param scope the scope of the change.
+	 * @param stage one of the valid stages for this object. See {@link #getAllStages()}
+	 */
 	void infer(ChangeScope scope, String stage) {
 		if (scope == null) {
 			throw new IllegalArgumentException('Scope cannot be null.')
@@ -83,6 +154,12 @@ class InferredVersion {
 		}
 	}
 
+	/**
+	 * Gets all stages considered valid by this object. Includes all
+	 * values from {@link #untaggedStages} and {@link taggedStages}, plus
+	 * {@code final}.
+	 * @return all valid stages to be used in {@link infer(String, String)}
+	 */
 	SortedSet<String> getAllStages() {
 		return untaggedStages + taggedStages + ['final']
 	}
