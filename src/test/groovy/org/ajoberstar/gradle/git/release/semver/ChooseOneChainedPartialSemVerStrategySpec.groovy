620243a476bb661a3706c34c6d07a914455e8a4c
Andrew Oberstar <ajoberstar@gmail.com>
2014-09-24 22:27:06 -0500

    Adding test for ChooseOne

--- src/test/groovy/org/ajoberstar/gradle/git/release/semver/ChooseOneChainedPartialSemVerStrategySpec.groovy
+++ src/test/groovy/org/ajoberstar/gradle/git/release/semver/ChooseOneChainedPartialSemVerStrategySpec.groovy
@@ -17,19 +17,20 @@ package org.ajoberstar.gradle.git.release.semver
 
 import spock.lang.Specification
 
-class ApplyAllChainedPartialSemVerStrategySpec extends Specification {
-	def 'infer correctly applies all strategies in order'() {
+class ChooseOneChainedPartialSemVerStrategySpec extends Specification {
+	def 'infer correctly picks the first strategy that changes state'() {
 		given:
 		def initialState = new SemVerStrategyState([:])
-		def chain = StrategyUtil.all(strategyAppend('a'), strategyAppend('b'), strategyAppend('c'))
+		def chain = StrategyUtil.one(nothing(), string('a'), string('b'), nothing())
 		expect:
-		chain.infer(initialState) == new SemVerStrategyState(inferredPreRelease: 'a.b.c')
+		chain.infer(initialState) == new SemVerStrategyState(inferredPreRelease: 'a')
 	}
 
-	private def strategyAppend(String str) {
-		return StrategyUtil.closure { state ->
-			def preRelease = state.inferredPreRelease ? "${state.inferredPreRelease}.${str}" : str
-			return state.copyWith(inferredPreRelease: preRelease)
-		}
+	private def nothing() {
+		return StrategyUtil.closure { state -> state }
+	}
+
+	private def string(String str) {
+		return StrategyUtil.closure { state -> state.copyWith(inferredPreRelease: str) }
 	}
 }
