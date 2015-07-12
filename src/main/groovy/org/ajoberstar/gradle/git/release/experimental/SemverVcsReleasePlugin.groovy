d4abdfb094e714df2acb17e9a2328862eb80964b
Andrew Oberstar <ajoberstar@gmail.com>
2015-07-11 23:14:11 -0500

    Initial version of experimental semver release plugin.

---
+++ src/main/groovy/org/ajoberstar/gradle/git/release/experimental/SemverVcsReleasePlugin.groovy
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ajoberstar.gradle.git.release.base
+
+import org.gradle.api.GradleException
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ *
+ */
+class SemverVcsReleasePlugin implements Plugin<Project> {
+	private static final Logger logger = LoggerFactory.getLogger(SemverVcsReleasePlugin)
+	private static final String PREPARE_TASK_NAME = 'prepare'
+	private static final String RELEASE_TASK_NAME = 'release'
+
+	void apply(Project project) {
+		project.plugins.apply('org.ajoberstar.grgit')
+		addPrepareTask(project, extension)
+		addReleaseTask(project, extension)
+	}
+
+	private void addPrepareTask(Project project, ReleasePluginExtension extension) {
+		project.tasks.create(PREPARE_TASK_NAME) {
+			description = 'Verifies that the project could be released.'
+			doLast {
+				logger.info('Fetching changes from remote.')
+				project.grgit.fetch()
+
+				if (project.grgit.branch.status(branch: project.grgit.branch.current).behindCount > 0) {
+					throw new GradleException('Current branch is behind the tracked branch. Cannot release.')
+				}
+			}
+		}
+
+		project.tasks.all { task ->
+			if (name != PREPARE_TASK_NAME) {
+				task.shouldRunAfter PREPARE_TASK_NAME
+			}
+		}
+	}
+
+	private void addReleaseTask(Project project, ReleasePluginExtension extension) {
+		project.tasks.create(RELEASE_TASK_NAME) {
+			description = 'Releases this project.'
+			dependsOn PREPARE_TASK_NAME
+			doLast {
+				ext.toPush = [project.grgit.branch.current.fullName]
+
+				// force version inference if it hasn't happened already
+				ext.tagName = project.version.toString()
+				if (tagName) {
+					toPush << tagName
+				}
+
+				logger.warn('Pushing changes in {} to remote.', toPush)
+				project.grgit.push(refsOrSpecs: toPush)
+			}
+		}
+	}
+}
