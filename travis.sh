300ed13f68b0984bc8a8da6fa3f0ef7b167426dc
Andrew Oberstar <ajoberstar@gmail.com>
2016-02-11 19:36:45 -0600

    Adding sonarqube pull request support

---
+++ travis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+export TERM=dumb
+
+if [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+	echo 'Running Gradle check with SonarQube analysis'
+	./gradlew clean check sonarqube \
+		-Dsonar.host.url=http://sonarqube.ajoberstar.com \
+		-Dsonar.login=$SONARQUBE_TOKEN
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN:-}" ]; then
+	echo 'Running Gradle check with SonarQube preview for pull requests'
+	./gradlew clean check sonarqube \
+		-Dsonar.host.url=http://sonarqube.ajoberstar.com \
+		-Dsonar.login=$SONARQUBE_TOKEN \
+		-Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+        -Dsonar.github.oauth=$GITHUB_TOKEN \
+        -Dsonar.analysis.mode=issues
+else
+	echo 'Running Gradle check without SonarQube analysis'
+	./gradlew clean check
+fi
