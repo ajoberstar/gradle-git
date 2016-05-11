d4e640d19eafdd22705be556e2341e551d031d29
Andrew Oberstar <ajoberstar@gmail.com>
2016-05-10 20:30:54 -0500

    Fixing reference to repo

--- src/main/groovy/org/ajoberstar/gradle/git/ghpages/GithubPagesPlugin.groovy
+++ src/main/groovy/org/ajoberstar/gradle/git/ghpages/GithubPagesPlugin.groovy
@@ -90,7 +90,7 @@ class GithubPagesPlugin implements Plugin<Project> {
 			doLast {
 				project.tasks."${PREPARE_TASK_NAME}".repo.with {
 					add(patterns: ['.'])
-					if (grgit.status().clean) {
+					if (status().clean) {
 						println 'Nothing to commit, skipping publish.'
 					} else {
 						commit(message: extension.commitMessage)
