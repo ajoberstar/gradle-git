c481d5369c82809a0785915ad8eb8dd03a620890
Serban Iordache <iordache@hotmail.com>
2016-08-24 22:39:14 +0200

    github-pages: fix patterns used for removing files

--- src/main/groovy/org/ajoberstar/gradle/git/ghpages/GithubPagesPlugin.groovy
+++ src/main/groovy/org/ajoberstar/gradle/git/ghpages/GithubPagesPlugin.groovy
@@ -97,10 +97,17 @@ class GithubPagesPlugin implements Plugin<Project> {
                     }
                 }
 
-                def targetDir = new File(extension.workingDir, extension.pages.relativeDestinationDir)
-                def filesList = targetDir.list { dir, name -> !name.equals('.git') }
-                if (filesList && extension.deleteExistingFiles) {
-                    repo.remove(patterns: filesList)
+                def relDestDir = extension.pages.relativeDestinationDir
+                def targetDir = new File(extension.workingDir, relDestDir)
+                if (extension.deleteExistingFiles) {
+                    def filesList = targetDir.list { dir, name -> !name.equals('.git') }
+                    if(filesList) {
+                        def removePatterns = filesList
+                        if(relDestDir && relDestDir != '.') {
+                            removePatterns = filesList.collect {name -> "$relDestDir/$name"}
+                        }
+                        repo.remove(patterns: removePatterns)
+                    }
                 }
                 ext.repo = repo
             }
